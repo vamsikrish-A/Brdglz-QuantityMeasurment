ce6483ac89afe8fc2bdc460fffa6931724747424
vamsikrish-A <krishnavamsi165@gmail.com>
2021-10-31 17:25:32 +0530

    Added:test case for valuecheck

--- src/test/java/qualitymeasurement/QualityMeasurementTest.java
+++ src/test/java/qualitymeasurement/QualityMeasurementTest.java
@@ -8,19 +8,24 @@ public class QualityMeasurementTest {
     public void given0FeetAnd0Feet_ShouldReturnEqual () {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(0.0);
-        Assertions.assertEquals(feet1,feet2);
+        Assertions.assertEquals(feet1, feet2);
     }
     @Test
     public void given0FeetAnd1Feet_ShouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
-        Assertions.assertNotEquals(feet1,feet2);
+        Assertions.assertNotEquals(feet1, feet2);
     }
 
     @Test
     public void givenNullValue_ShouldReturnFalse() {
         Feet feet1 = new Feet(0.0);
         Assertions.assertNotNull(feet1);
+    }
+    @Test
+    public void givenThisValueAndValue_ShouldReturnNotEquals() {
+        Feet feet1 = new Feet(0.0);
+        Assertions.assertNotEquals(feet1, 0);
     }
 
 
